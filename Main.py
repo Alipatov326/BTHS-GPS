6f67d30725ce8b3c2f028ad7bc0b8866382d0b8f
Alipatov326 <155419066+Alipatov326@users.noreply.github.com>
2025-05-23 11:50:41 -0400

    Update map

--- Main.py
+++ Main.py
@@ -52,23 +52,21 @@ class Graph:
 
 if __name__ == '__main__':
 
+# School mapped out with nodes
     g = Graph()
 
     #floor 0 (basement)
     g.add_vertex('A0')
     g.add_vertex('B0')
-    g.add_vertex('C0')
     g.add_vertex('D0')
     g.add_vertex('E0')
     g.add_vertex('F0')
 
     #floor 0 connections
-    g.add_edge('B', 'C', 0)
-    g.add_edge('C', 'D', 0)
-    g.add_edge('D', 'E', 0)
-    g.add_edge('E', 'F', 0)
-    g.add_edge('F', 'A', 0)
-    g.add_edge('A', 'B', 0)
+    g.add_edge('D0', 'E0', 39)
+    g.add_edge('E0', 'F0', 43)
+    g.add_edge('F0', 'A0', 58)
+    g.add_edge('A0', 'B0', 40)
 
     #floor 1
     g.add_vertex('A1')
@@ -79,12 +77,13 @@ if __name__ == '__main__':
     g.add_vertex('F1')
 
     #floor 1 connections
-    g.add_edge('B', 'C', 0)
-    g.add_edge('C', 'D', 0)
-    g.add_edge('D', 'E', 0)
-    g.add_edge('E', 'F', 0)
-    g.add_edge('F', 'A', 0)
-    g.add_edge('A', 'B', 0)
+    g.add_edge('B1', 'C1', 50)
+    g.add_edge('C1', 'D1', 53)
+    g.add_edge('D1', 'E1', 39)
+    g.add_edge('E1', 'F1', 53)
+    g.add_edge('F1', 'A1', 58)
+    g.add_edge('A1', 'B1', 40)
+    g.add_edge('C1', 'F1', 39)
 
     #floor 2
     g.add_vertex('A2')
@@ -95,12 +94,13 @@ if __name__ == '__main__':
     g.add_vertex('F2')
 
     #floor 2 connections
-    g.add_edge('B', 'C', 0)
-    g.add_edge('C', 'D', 0)
-    g.add_edge('D', 'E', 0)
-    g.add_edge('E', 'F', 0)
-    g.add_edge('F', 'A', 0)
-    g.add_edge('A', 'B', 0)
+    g.add_edge('B2', 'C2', 50)
+    g.add_edge('C2', 'D2', 53)
+    g.add_edge('D2', 'E2', 39)
+    g.add_edge('E2', 'F2', 53)
+    g.add_edge('F2', 'A2', 58)
+    g.add_edge('A2', 'B2', 40)
+    g.add_edge('C2', 'F2', 39)
 
     #floor 3
     g.add_vertex('A3')
@@ -111,12 +111,13 @@ if __name__ == '__main__':
     g.add_vertex('F3')
 
     #floor 3 connections
-    g.add_edge('B', 'C', 0)
-    g.add_edge('C', 'D', 0)
-    g.add_edge('D', 'E', 0)
-    g.add_edge('E', 'F', 0)
-    g.add_edge('F', 'A', 0)
-    g.add_edge('A', 'B', 0)
+    g.add_edge('B3', 'C3', 50)
+    g.add_edge('C3', 'D3', 53)
+    g.add_edge('D3', 'E3', 39)
+    g.add_edge('E3', 'F3', 53)
+    g.add_edge('F3', 'A3', 58)
+    g.add_edge('A3', 'B3', 40)
+    g.add_edge('C3', 'F3', 39)
 
     #floor 4
     g.add_vertex('A4')
@@ -127,12 +128,13 @@ if __name__ == '__main__':
     g.add_vertex('F4')
 
     #floor 4 connections
-    g.add_edge('B', 'C', 0)
-    g.add_edge('C', 'D', 0)
-    g.add_edge('D', 'E', 0)
-    g.add_edge('E', 'F', 0)
-    g.add_edge('F', 'A', 0)
-    g.add_edge('A', 'B', 0)
+    g.add_edge('B4', 'C4', 50)
+    g.add_edge('C4', 'D4', 53)
+    g.add_edge('D4', 'E4', 39)
+    g.add_edge('E4', 'F4', 53)
+    g.add_edge('F4', 'A4', 58)
+    g.add_edge('A4', 'B4', 40)
+    g.add_edge('C4', 'F4', 39)
 
     #floor 5
     g.add_vertex('A5')
@@ -143,12 +145,12 @@ if __name__ == '__main__':
     g.add_vertex('F5')
 
     #floor 5 connections
-    g.add_edge('B', 'C', 0)
-    g.add_edge('C', 'D', 0)
-    g.add_edge('D', 'E', 0)
-    g.add_edge('E', 'F', 0)
-    g.add_edge('F', 'A', 0)
-    g.add_edge('A', 'B', 0)
+    g.add_edge('B5', 'C5', 50)
+    g.add_edge('C5', 'D5', 53)
+    g.add_edge('D5', 'E5', 39)
+    g.add_edge('E5', 'F5', 53)
+    g.add_edge('F5', 'A5', 58)
+    g.add_edge('A5', 'B5', 40)
 
     #floor 6
     g.add_vertex('A6')
@@ -159,12 +161,13 @@ if __name__ == '__main__':
     g.add_vertex('F6')
 
     #floor 6 connections
-    g.add_edge('B', 'C', 0)
-    g.add_edge('C', 'D', 0)
-    g.add_edge('D', 'E', 0)
-    g.add_edge('E', 'F', 0)
-    g.add_edge('F', 'A', 0)
-    g.add_edge('A', 'B', 0)
+    g.add_edge('B6', 'C6', 50)
+    g.add_edge('C6', 'D6', 53)
+    g.add_edge('D6', 'E6', 39)
+    g.add_edge('E6', 'F6', 53)
+    g.add_edge('F6', 'A6', 58)
+    g.add_edge('A6', 'B6', 40)
+    g.add_edge('C6', 'F6', 39)
 
     #floor 7
     g.add_vertex('A7')
@@ -175,34 +178,94 @@ if __name__ == '__main__':
     g.add_vertex('F7')
 
     #floor 7 connections
-    g.add_edge('B', 'C', 0)
-    g.add_edge('C', 'D', 0)
-    g.add_edge('D', 'E', 0)
-    g.add_edge('E', 'F', 0)
-    g.add_edge('F', 'A', 0)
-    g.add_edge('A', 'B', 0)
+    g.add_edge('B7', 'C7', 50)
+    g.add_edge('C7', 'D7', 53)
+    g.add_edge('D7', 'E7', 39)
+    g.add_edge('E7', 'F7', 53)
+    g.add_edge('F7', 'A7', 58)
+    g.add_edge('A7', 'B7', 40)
+    g.add_edge('C7', 'F7', 39)
+
+    #floor 8
+
+    g.add_vertex('C8')
+    g.add_vertex('F8')
+
+    #floor 8 connections
+    g.add_edge('C7', 'C8', 12)
+    g.add_edge('F7', 'F8', 12)
+    g.add_edge('C8', 'F8', 39)
+
+    #stairs (change length later)
+
+    #A stairs
+    g.add_edge('A0', 'A1', 12)
+    g.add_edge('A1', 'A2', 12)
+    g.add_edge('A2', 'A3', 12)
+    g.add_edge('A3', 'A4', 12)
+    g.add_edge('A4', 'A5', 12)
+    g.add_edge('A5', 'A6', 12)
+    g.add_edge('A6', 'A7', 12)
+    
+    #B stairs
+    g.add_edge('B0', 'B1', 12)
+    g.add_edge('B1', 'B2', 12)
+    g.add_edge('B2', 'B3', 12)
+    g.add_edge('B3', 'B4', 12)
+    g.add_edge('B4', 'B5', 12)
+    g.add_edge('B5', 'B6', 12)
+    g.add_edge('B6', 'B7', 12)
+
+    #C stairs
+    g.add_edge('C1', 'C2', 12)
+    g.add_edge('C2', 'C3', 12)
+    g.add_edge('C3', 'C4', 12)
+    g.add_edge('C4', 'C5', 12)
+    g.add_edge('C5', 'C6', 12)
+
+    #D stairs
+    g.add_edge('D0', 'D1', 12)
+    g.add_edge('D1', 'D2', 12)
+    g.add_edge('D2', 'D3', 12)
+    g.add_edge('D3', 'D4', 12)
+    g.add_edge('D4', 'D5', 12)
+    g.add_edge('D5', 'D6', 12)
+    g.add_edge('D6', 'D7', 12)
+
+    #E stairs
+    g.add_edge('E0', 'E1', 12)
+    g.add_edge('E1', 'E2', 12)
+    g.add_edge('E2', 'E3', 12)
+    g.add_edge('E3', 'E4', 12)
+    g.add_edge('E4', 'E5', 12)
+    g.add_edge('E5', 'E6', 12)
+    g.add_edge('E6', 'E7', 12)
+
+    #F stairs
+    g.add_edge('F0', 'F1', 12)
+    g.add_edge('F1', 'F2', 12)
+    g.add_edge('F2', 'F3', 12)
+    g.add_edge('F3', 'F4', 12)
+    g.add_edge('F4', 'F5', 12)
+    g.add_edge('F5', 'F6', 12)
+
+    #Elevators (distance tbd)
+
+    #student ones
+
+    
 
     #floor 8 (to be added with 9th floor chorus room)
-
-
-
-    #floor  connections
-    g.add_edge('B', 'C', 0)
-    g.add_edge('C', 'D', 0)
-    g.add_edge('D', 'E', 0)
-    g.add_edge('E', 'F', 0)
-    g.add_edge('F', 'A', 0)
-    g.add_edge('A', 'B', 0)
 
 
     for v in g:
         for w in v.get_connections():
             vid = v.get_id()
             wid = w.get_id()
-            print '( %s , %s, %3d)'  % ( vid, wid, v.get_weight(w))
+            print ('( %s , %s, %3d)'  % ( vid, wid, v.get_weight(w)))
 
     for v in g:
-        print 'g.vert_dict[%s]=%s' %(v.get_id(), g.vert_dict[v.get_id()])
+        print ('g.vert_dict[%s]=%s' %(v.get_id(), g.vert_dict[v.get_id()]))
 
 
 
